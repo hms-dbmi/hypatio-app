f4b67a5d1c7219fd2f2133deaefc7ec842a6645e
mtmcduffie <mcduffie.michael@gmail.com>
2016-11-11 17:20:43 -0500

    Implementing security stuff.

--- dataprojects/models.py
+++ dataprojects/models.py
@@ -1,8 +1,11 @@
 from django.db import models
 
+
 class DataProject(models.Model):
     name = models.CharField(max_length=255, blank=True, null=True, verbose_name="Name of project")
     institution = models.CharField(max_length=255, blank=True, null=True, verbose_name="Institution")
     description = models.CharField(max_length=4000, blank=True, null=True, verbose_name="Description")
     short_description = models.CharField(max_length=255, blank=True, null=True, verbose_name="Short Description")
-
+    project_key = models.CharField(max_length=100, blank=True, null=True, verbose_name="Project Key")
+    permission_scheme = models.CharField(max_length=100, blank=True, null=True, verbose_name="Permission Scheme")
+    project_url = models.CharField(max_length=255, blank=True, null=True, verbose_name="Project URL")
