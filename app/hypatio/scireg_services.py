370cf91033801e01adb0c3c888f79e29253c7203
Nate Bessa <natebessa@gmail.com>
2017-12-19 10:30:33 -0500

    Brought in some DUA and project related features that were removed from SciAuthZ
    Added DUA and DUA Sign models that were removed from SciAuthZ. Revised DataProject model to include a project supervisor and some permission fields previously in SciAuthZ as well. Added HTML DUA forms. Checking JWT for expiration and forcing login if expired. Removed URLs from settings that no longer exist in SciAuthZ. Enabled admin site and added new models to it.  Refactored Access_Request.html by making DUA form selection happen regardless of number of DUA form options. Refactored Blurb.html by bringing in more permissions information into the dictionary that the template loops on.

--- app/hypatio/scireg_services.py
+++ app/hypatio/scireg_services.py
@@ -4,14 +4,3 @@ from django.conf import settings
 
 def build_headers_with_jwt(user_jwt):
     return {"Authorization": "JWT " + user_jwt, 'Content-Type': 'application/json'}
-
-
-def request_project_access(user_jwt, project_key):
-
-    jwt_headers = build_headers_with_jwt(user_jwt)
-
-    project_permissions_setup = settings.PROJECT_SETUP_URL + project_key
-
-    r = requests.get(project_permissions_setup, headers=jwt_headers)
-
-    return r
