a2db11cb03a0578699ddb02cb174a31b3346e45e
Nate Bessa <natebessa@gmail.com>
2018-01-10 11:42:55 -0500

    TC-50: Implemented registration sign up for new users
    If a user hit hypatio for the first time, a user gets created for them in scireg automatically, but not the registration form. The profile page will not prompt them to complete their registration and then it will send that data to scireg to be created as a new Registration object. Also added functionality to make the first and last name readonly fields if the user already has a registration.

--- app/profile/views.py
+++ app/profile/views.py
@@ -9,7 +9,7 @@ from django.contrib.auth import logout
 from django.shortcuts import render, redirect
 from pyauth0jwt.auth0authenticate import user_auth_and_jwt, validate_jwt, logout_redirect
 
-from .forms import UserProfileForm
+from .forms import RegistrationForm
 
 from django.http import HttpResponse, HttpResponseRedirect
 from django.urls import reverse
@@ -38,46 +38,48 @@ def profile(request, template_name='profile/profile.html'):
     if request.method == 'POST':
 
         # Process the form
-        form = UserProfileForm(request.POST)
+        form = RegistrationForm(request.POST)
         if form.is_valid():
+            logger.debug('[HYPATIO][DEBUG] Profile form fields submitted: ' + json.dumps(form.cleaned_data))
 
-            # The SciReg API endpoint
-            registration_url = settings.SCIREG_SERVER_URL + '/api/register/' + form.cleaned_data['id'] + '/'
-
-            logger.debug('[HYPATIO][DEBUG] Profile form fields submitted: ' + json.dumps(form.cleaned_data))            
-
-            # Send the data to SciReg
-            requests.put(registration_url, headers=jwt_headers, data=json.dumps(form.cleaned_data), verify=False)
+            # Create a new registration with a POST
+            if form.cleaned_data['id'] == "":
+                registration_url = settings.SCIREG_SERVER_URL + '/api/register/'
+                requests.post(registration_url, headers=jwt_headers, data=json.dumps(form.cleaned_data), verify=False)
+            # Update an existing registration with a PUT to the specific ID
+            else:
+                registration_url = settings.SCIREG_SERVER_URL + '/api/register/' + form.cleaned_data['id'] + '/'
+                requests.put(registration_url, headers=jwt_headers, data=json.dumps(form.cleaned_data), verify=False)
 
             # Generate and render the form.
             return render(request, template_name, {'form': form,
                                                    'user': user,
+                                                   'new_user': False,
                                                    'user_logged_in': user_logged_in})
         else:
             logger.debug('[HYPATIO][DEBUG] Profile form errors: ' + form.errors.as_json())
 
-            # TODO send this back to profile to display errors
+            # TODO Not implemented
             return HttpResponse(status=500)
 
     else:
 
-        # The SciReg API endpoint
-        registration_url = settings.SCIREG_SERVER_URL + '/api/register/'
-
         # Query SciReg to get the user's information
+        registration_url = settings.SCIREG_SERVER_URL + '/api/register/'
         registration_info = requests.get(registration_url, headers=jwt_headers, verify=False).json()
-
-        if registration_info is not None:
-            registration_info = registration_info["results"]
-        else:
-            # TODO user does not have a registration in scireg, what should we do?
-            pass
-
         logger.debug('[HYPATIO][DEBUG] Registration info ' + json.dumps(registration_info))
 
+        if registration_info['count'] != 0:
+            registration_info = registration_info["results"][0]
+            form = RegistrationForm(initial=registration_info)
+            new_user = False
+        else:
+            # User does not have a registration in scireg, present them with a blank form to complete and prepopulate the email
+            form = RegistrationForm(initial={'email': user.email}, new_registration=True)
+            new_user = True
+
         # Generate and render the form.
-        form = UserProfileForm(initial=registration_info[0])
         return render(request, template_name, {'form': form,
                                                'user': user,
+                                               'new_user': new_user,
                                                'user_logged_in': user_logged_in})
-
