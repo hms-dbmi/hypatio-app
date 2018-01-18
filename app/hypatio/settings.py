5ee73d317d497a8b62f31536da2eccde6c278e8c
Bryan Larson <bryan_larson@hms.harvard.edu>
2018-01-18 11:45:28 -0500

    TC-51 - Updated the login URL to request email confirmation

--- app/hypatio/settings.py
+++ app/hypatio/settings.py
@@ -15,7 +15,6 @@ import sys
 
 from os.path import normpath, join, dirname, abspath
 from django.utils.crypto import get_random_string
-from pythonpstore.pythonpstore import SecretStore
 
 chars = 'abcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*(-_=+)'
 
@@ -31,15 +30,9 @@ SECRET_KEY = os.environ.get("SECRET_KEY", get_random_string(50, chars))
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-secret_store = SecretStore()
-PARAMETER_PATH = os.environ.get("PS_PATH", "")
+PROJECT = 'hypatio'
 
-if PARAMETER_PATH:
-    ALLOWED_HOSTS = [secret_store.get_secret_for_key(PARAMETER_PATH + '.allowed_hosts')]
-    RAVEN_URL = secret_store.get_secret_for_key(PARAMETER_PATH + '.raven_url')
-else:
-    ALLOWED_HOSTS = ["localhost"]
-    RAVEN_URL = ""
+ALLOWED_HOSTS = [os.environ.get("ALLOWED_HOSTS")]
 
 # Application definition
 
@@ -234,7 +227,7 @@ LOGGING = {
 }
 
 RAVEN_CONFIG = {
-    'dsn': RAVEN_URL,
+    'dsn': os.environ.get("RAVEN_URL", ""),
     # If you are using git, you can also automatically configure the
     # release based on the git info.
     'release': '1',
