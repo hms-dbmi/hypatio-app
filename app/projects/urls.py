cb292c51e8779c912890c2d49c8d641d1c67eed8
Nate Bessa <natebessa@gmail.com>
2018-01-30 15:37:49 -0500

    TC-79: Adds step to project page for sending a permission request
    Future goal: this request should be automatically made when all prior steps (forms and joining a team) are completed.

--- app/projects/urls.py
+++ app/projects/urls.py
@@ -3,7 +3,7 @@ from django.conf.urls import url
 from .views import list_data_projects
 from .views import list_data_contests
 from .views import request_access
-from .views import submit_request
+from .views import submit_user_permission_request
 from .views import manage_contests
 from .views import save_signed_agreement_form
 from .views import grant_access_with_view_permissions
@@ -15,7 +15,7 @@ urlpatterns = (
     url(r'^list_data_projects/$', list_data_projects),
     url(r'^list_data_contests/$', list_data_contests),
     url(r'^request_access/$', request_access),
-    url(r'^submit_request/$', submit_request),
+    url(r'^submit_user_permission_request/$', submit_user_permission_request),
     url(r'^managecontests/$', manage_contests),
     url(r'^grantviewpermissions', grant_access_with_view_permissions),
     url(r'^save_signed_agreement_form', save_signed_agreement_form),
