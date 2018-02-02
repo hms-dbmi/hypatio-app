1fb4087e6dbab5de450ab4180c9c57dfb27e3943
Nate Bessa <natebessa@gmail.com>
2018-02-02 09:54:05 -0500

    TC-99: adds country to the profile as a required field

--- app/profile/forms.py
+++ app/profile/forms.py
@@ -1,5 +1,6 @@
 from django import forms
 from django.utils.safestring import mark_safe
+from django_countries.data import COUNTRIES
 
 class RegistrationForm(forms.Form):
 
@@ -29,6 +30,7 @@ class RegistrationForm(forms.Form):
     city = forms.CharField(label="City", max_length=255, required=False)
     state = forms.CharField(label="State", max_length=255, required=False)
     zipcode = forms.CharField(label="Zip", max_length=255, required=False)
+    country = forms.ChoiceField(sorted(COUNTRIES.items()), label="Country", required=True)
     phone_number = forms.CharField(label="Phone Number", max_length=255, required=False)
 
     twitter_handle = forms.CharField(label="Twitter Handle", max_length=255, initial="", required=False)
