dbeb6e19450740ce261ce56703df2bbbeba2c398
Nate Bessa <natebessa@gmail.com>
2018-02-13 15:46:19 -0500

    TC-110: Adds contest management screen and associated functionality
    Adds the contest management screen, which displays teams and team info on a modal. Adds a migration for giving agreement forms a short name (displayed on contest management screen). Adds another migration that adds a status field to the Team model. Adds views/URLs for changing team status (finalize, activate, deactivate). Removes some legacy code for granting VIEW permissions since that must now be done at the point of a contest manager activating a team. NOTE: code still needed to grant or revoke VIEW permissions based on team status changes.

--- app/projects/urls.py
+++ app/projects/urls.py
@@ -4,16 +4,19 @@ from .views import list_data_projects
 from .views import list_data_contests
 from .views import request_access
 from .views import submit_user_permission_request
-from .views import manage_contests
+from .views import manage_contest
 from .views import save_signed_agreement_form
-from .views import grant_access_with_view_permissions
 from .views import project_details
 from .views import signout
+from .views import view_team_management
 from .views_teams import join_team
 from .views_teams import create_team_from_pi
 from .views_teams import team_signup_form
 from .views_teams import approve_team_join
 from .views_teams import reject_team_join
+from .views_teams import finalize_team
+from .views_teams import activate_team
+from .views_teams import deactivate_team
 
 urlpatterns = (
     url(r'^$', list_data_projects),
@@ -21,14 +24,17 @@ urlpatterns = (
     url(r'^list_data_contests/$', list_data_contests),
     url(r'^request_access/$', request_access),
     url(r'^submit_user_permission_request/$', submit_user_permission_request),
-    url(r'^managecontests/$', manage_contests),
-    url(r'^grantviewpermissions', grant_access_with_view_permissions),
+    url(r'^manage/(?P<project_key>[^/]+)/$', manage_contest),
+    url(r'^view_team_management/$', view_team_management),
     url(r'^save_signed_agreement_form', save_signed_agreement_form),
     url(r'^signout/$', signout),
     url(r'^join_team/$', join_team),
     url(r'^approve_team_join/$', approve_team_join),
     url(r'^reject_team_join/$', reject_team_join),
     url(r'^create_team_from_pi/$', create_team_from_pi),
+    url(r'^finalize_team/$', finalize_team),
+    url(r'^activate_team/$', activate_team),
+    url(r'^deactivate_team/$', deactivate_team),
     url(r'^team_signup_form/(P<project_key>[^/]+)/$', team_signup_form),
     url(r'^(?P<project_key>[^/]+)/$', project_details)
 )
