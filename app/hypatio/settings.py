3bd6d835b85dfa6c2f7aca22385442d1925cd6b5
mtmcduffie <mcduffie.michael@gmail.com>
2018-02-19 15:18:49 -0500

    Adding SciAuth URL, it's repeated with different names. Fix this later...

--- app/hypatio/settings.py
+++ app/hypatio/settings.py
@@ -125,6 +125,7 @@ AUTH0_LOGOUT_URL = os.environ.get("AUTH0_LOGOUT_URL","")
 
 AUTHENTICATION_BACKENDS = ['pyauth0jwt.auth0authenticate.Auth0Authentication', 'django.contrib.auth.backends.ModelBackend']
 
+AUTHENTICATION_LOGIN_URL = os.environ.get("ACCOUNT_SERVER_URL")
 ACCOUNT_SERVER_URL = os.environ.get("ACCOUNT_SERVER_URL")
 SCIREG_SERVER_URL = os.environ.get("SCIREG_SERVER_URL", "")
 AUTHZ_BASE = os.environ.get("AUTHZ_BASE", "")
