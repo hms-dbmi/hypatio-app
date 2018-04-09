c61d1848a18c94d49b5fc27a609d94e726908aad
Bryan Larson <b32147@gmail.com>
2018-04-09 10:13:47 -0400

    TC-186 - Implemented dataset uploading; updated models to store submissions

--- app/hypatio/settings.py
+++ app/hypatio/settings.py
@@ -218,6 +218,20 @@ EMAIL_HOST_USER = os.environ.get("EMAIL_HOST_USER")
 EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_HOST_PASSWORD")
 EMAIL_PORT = os.environ.get("EMAIL_PORT")
 
+
+#####################################################################################
+# FileService Configurations
+#####################################################################################
+
+FILESERVICE_API_URL = os.environ.get('FILESERVICE_API_URL')
+FILESERVICE_GROUP = os.environ.get('FILESERVICE_GROUP')
+FILESERVICE_AWS_BUCKET = os.environ.get('S3_BUCKET')
+FILESERVICE_SERVICE_ACCOUNT = 'hypatio'
+FILESERVICE_SERVICE_TOKEN = os.environ.get('FILESERVICE_SERVICE_TOKEN')
+
+#####################################################################################
+
+
 LOGGING = {
     'version': 1,
     'handlers': {
