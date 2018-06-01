e7fd6f1f4863246807b8948a34ea93e5f99a3bbd
Nate Bessa <natebessa@gmail.com>
2018-06-01 14:26:00 -0400

    PPM-228: Adds AUTH0_CLIENT_ID_LIST setting

--- app/hypatio/settings.py
+++ app/hypatio/settings.py
@@ -120,7 +120,8 @@ AUTH_PASSWORD_VALIDATORS = [
 SITE_URL = os.environ.get("SITE_URL")
 
 AUTH0_DOMAIN = os.environ.get("AUTH0_DOMAIN")
-AUTH0_CLIENT_ID = os.environ.get("AUTH0_CLIENT_ID")
+AUTH0_CLIENT_ID = os.environ.get("AUTH0_CLIENT_ID") # TODO remove once py-auth0-jwt(-rest) package(s) are upgraded
+AUTH0_CLIENT_ID_LIST = os.environ.get("AUTH0_CLIENT_ID_LIST")
 AUTH0_SECRET = os.environ.get("AUTH0_SECRET")
 AUTH0_SUCCESS_URL = os.environ.get("AUTH0_SUCCESS_URL")
 AUTH0_LOGOUT_URL = os.environ.get("AUTH0_LOGOUT_URL","")
