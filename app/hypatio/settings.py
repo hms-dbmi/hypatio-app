1697ac2a1ad808d55c03912df0fb8b7e8220b259
Nate Bessa <natebessa@gmail.com>
2018-06-04 10:47:58 -0400

    TC-228: Split function needed to turn string into list

--- app/hypatio/settings.py
+++ app/hypatio/settings.py
@@ -121,7 +121,7 @@ SITE_URL = os.environ.get("SITE_URL")
 
 AUTH0_DOMAIN = os.environ.get("AUTH0_DOMAIN")
 AUTH0_CLIENT_ID = os.environ.get("AUTH0_CLIENT_ID") # TODO remove once py-auth0-jwt(-rest) package(s) are upgraded
-AUTH0_CLIENT_ID_LIST = os.environ.get("AUTH0_CLIENT_ID_LIST")
+AUTH0_CLIENT_ID_LIST = os.environ.get("AUTH0_CLIENT_ID_LIST").split(",")
 AUTH0_SECRET = os.environ.get("AUTH0_SECRET")
 AUTH0_SUCCESS_URL = os.environ.get("AUTH0_SUCCESS_URL")
 AUTH0_LOGOUT_URL = os.environ.get("AUTH0_LOGOUT_URL","")
