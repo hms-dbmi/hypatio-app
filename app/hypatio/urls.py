245feebc4a79aa7703cbfe80317d86b5bf2dbe89
Nate Bessa <natebessa@gmail.com>
2018-07-20 15:58:26 -0400

    HYP-18: Improve some of the link names

--- app/hypatio/urls.py
+++ app/hypatio/urls.py
@@ -1,13 +1,19 @@
-from django.conf.urls import url, include
+from django.conf import settings
+from django.conf.urls import include
+from django.conf.urls import url
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.views.defaults import page_not_found
-from django.conf import settings
-from django.conf.urls.static import static
+
+from projects.views import list_data_projects
+from projects.views import list_data_challenges
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^contact/', include('contact.urls')),
     url(r'^projects/', include('projects.urls')),
     url(r'^profile/', include('profile.urls')),
+    url(r'^data-sets/$', list_data_projects),
+    url(r'^data-challenges/$', list_data_challenges),
     url(r'^', include('projects.urls')),
 ]
