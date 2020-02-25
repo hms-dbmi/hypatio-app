e972818cd4c53b7e3e8a76755eaa46285d7b94d7
Bryan Larson <b32147@gmail.com>
2020-02-25 16:37:31 -0500

    HYP-184 - Updated URLs to make trailing slash optional

--- app/contact/urls.py
+++ app/contact/urls.py
@@ -3,6 +3,6 @@ from .views import contact_form
 
 app_name = 'contact'
 urlpatterns = (
-    url(r'^(?P<project_key>[^/]+)/$', contact_form, name='contact_form'),
+    url(r'^(?P<project_key>[^/]+)/?$', contact_form, name='contact_form'),
     url(r'^', contact_form, name='contact_form'),
 )
