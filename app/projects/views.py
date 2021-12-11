399618f59cdbd6283cae376162714be79f64410f
Bryan Larson <bryan_larson@hms.harvard.edu>
2021-12-11 08:44:17 -0500

    HYP-252 - Add signed agreement form fields to the agreement form view

--- app/projects/views.py
+++ app/projects/views.py
@@ -29,6 +29,7 @@ from projects.models import DataProject
 from projects.models import HostedFile
 from projects.models import Participant
 from projects.models import SignedAgreementForm
+from projects.models import MIMIC3SignedAgreementFormFields
 
 from projects.panels import SIGNUP_STEP_COMPLETED_STATUS
 from projects.panels import SIGNUP_STEP_CURRENT_STATUS
@@ -60,6 +61,19 @@ def signed_agreement_form(request):
     except ObjectDoesNotExist:
         participant = None
 
+    # Get fields, if applicable. It sucks that these are hard-coded but until we
+    # find a better solution and have more time, this is it.
+    signed_agreement_form_fields = {}
+
+    # Check MIMIC3 first.
+    fields = MIMIC3SignedAgreementFormFields.objects.filter(signed_agreement_form=signed_form).first()
+    if fields:
+
+        # Create a dictionary of the values
+        signed_agreement_form_fields = {
+            "Email Address": fields.email,
+        }
+
     if is_manager or signed_form.user == request.user:
         template_name = "projects/participate/view-signed-agreement-form.html"
         filled_out_signed_form = None
@@ -69,6 +83,7 @@ def signed_agreement_form(request):
                                                "is_manager": is_manager,
                                                "signed_form": signed_form,
                                                "filled_out_signed_form": filled_out_signed_form,
+                                               "signed_agreement_form_fields": signed_agreement_form_fields,
                                                "participant": participant})
     else:
         return HttpResponse(403)
