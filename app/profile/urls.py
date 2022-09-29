6eba10d3c2106f0a96da4bd9d7a8165abccbcd43
Bryan Larson <bryan_larson@hms.harvard.edu>
2022-09-29 09:08:40 -0600

    DBMSIVC-122 - Upgraded to Django 4.x

--- app/profile/urls.py
+++ app/profile/urls.py
@@ -1,4 +1,4 @@
-from django.conf.urls import url
+from django.urls import re_path
 
 from profile.views import profile
 from profile.views import send_confirmation_email_view
@@ -7,8 +7,8 @@ from profile.views import update_profile
 
 app_name = 'profile'
 urlpatterns = [
-    url(r'^$', profile, name='profile'),
-    url(r'^send_confirmation_email/$', send_confirmation_email_view, name='send_confirmation_email'),
-    url(r'^update/$', update_profile, name='update'),
-    url(r'^signout/$', signout, name='signout'),
+    re_path(r'^$', profile, name='profile'),
+    re_path(r'^send_confirmation_email/$', send_confirmation_email_view, name='send_confirmation_email'),
+    re_path(r'^update/$', update_profile, name='update'),
+    re_path(r'^signout/$', signout, name='signout'),
 ]
