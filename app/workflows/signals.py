b59059f651c7bd53cc22a5ba70284ae4e6e400d7
Bryan Larson <bryan_larson@hms.harvard.edu>
2025-07-22 11:42:13 -0600

    feat(workflows): Implemented process for step rejections

--- app/workflows/signals.py
+++ app/workflows/signals.py
@@ -6,21 +6,12 @@ from workflows.models import StepState
 from workflows.models import StepStateReview
 from workflows.models import StepStateInitialization
 
-deleting_workflow_states = set()
-
-@receiver(pre_delete, sender=WorkflowState)
-def workflow_state_pre_delete(sender, instance, **kwargs):
-    deleting_workflow_states.add(instance.id)
-
-@receiver(post_delete, sender=WorkflowState)
-def workflow_state_pre_delete(sender, instance, **kwargs):
-    deleting_workflow_states.remove(instance.id)
 
 @receiver(post_delete, sender=StepState)
 def step_state_post_delete(sender, instance, using, **kwargs):
 
-    # Ensure the workflow state object exists
-    if deleting_workflow_states and instance.workflow_state.id in deleting_workflow_states:
+    # Do not do this if we are deleting the WorkflowState
+    if kwargs.get("origin") and kwargs["origin"].model is WorkflowState:
         return
 
     # Re-create the step state
@@ -32,11 +23,24 @@ def step_state_post_delete(sender, instance, using, **kwargs):
 
 @receiver(post_delete, sender=StepStateReview)
 def step_state_review_post_delete(sender, instance, using, **kwargs):
-    # Update the workflow steps
-    instance.step_state.workflow_state.set_step_statuses()
+
+    # Do not do this if we are deleting the WorkflowState
+    if kwargs.get("origin") and kwargs["origin"].model is WorkflowState:
+        return
+
+    try:
+        # Update the workflow steps
+        instance.step_state.workflow_state.set_step_statuses()
+    except StepState.DoesNotExist:
+        pass
 
 
 @receiver(post_delete, sender=StepStateInitialization)
 def step_state_initialization_post_delete(sender, instance, using, **kwargs):
+
+    # Do not do this if we are deleting the WorkflowState
+    if kwargs.get("origin") and kwargs["origin"].model is WorkflowState:
+        return
+
     # Update the workflow steps
     instance.step_state.workflow_state.set_step_statuses()
